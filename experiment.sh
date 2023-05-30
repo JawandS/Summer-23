da2442eea382dad979222abbfb99049618445737
JawandS <jawandsingh@gmail.com>
2023-05-30 13:05:47 -0400

    change file names

--- experiment.sh
+++ experiment.sh
@@ -15,12 +15,12 @@ experiment() {
   fi
   truncate -s 0 raw.txt
   # run the workload and measure how long it took
-  stress-ng --cpu 1 --cpu-load 50 --timeout 10s --metrics --no-rand-seed >> Logs/log_"$1".txt 2>&1
+  stress-ng --cpu 1 --cpu-load 50 --timeout 10s --metrics --no-rand-seed >> Logs/"$1".txt 2>&1
   # end tracing
   killall -q bpftrace
   # update logs
   outputSize=$(wc -l raw.txt) # context switches recorded
-  echo "$outputSize" | tee -a Logs/log_"$1".txt
+  echo "$outputSize" | tee -a Logs/"$1".txt
 }
 # run experiment
 echo "Starting experiment ${1}"
@@ -28,10 +28,10 @@ iterationCounter=0
 for _ in {1..5}; do # number of iterations
   sleep 1
   # experiment phase
-  iterationCounter=$((iterationCounter + 1)) && printf "\t---------Run %s---------\n" "$iterationCounter" | tee -a Logs/log_"$1".txt
-  printf "\nBase Run\n" | tee -a Logs/log_"$1".txt
+  iterationCounter=$((iterationCounter + 1)) && printf "\t---------Run %s---------\n" "$iterationCounter" | tee -a Logs/"$1".txt
+  printf "\nBase Run\n" | tee -a Logs/"$1".txt
   experiment "$1" X # base run
-  printf "\nOne Context Switch\n" | tee -a Logs/log_"$1".txt
+  printf "\nOne Context Switch\n" | tee -a Logs/"$1".txt
   experiment "$1" 1 # 1 context switch
 done
 # git pull
