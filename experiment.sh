4fa99a9ec93e7152828f2092ce5c9ad4aaa8d946
JawandS <jawandsingh@gmail.com>
2023-05-30 11:49:59 -0400

    update exp

--- experiment.sh
+++ experiment.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
-# tracing overhead experiment
-
-# pull from git
-git pull
-
-# functin to reset
-resetExperiment() {
-    killall -q python3
-    killall -q bpftrace
-    truncate -s 0 trace_log.txt
+# start overhead
+# git pull
+# echo "$2" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor # powersave or performance
+# define experiment
+experiment() {
+  # setup
+  killall -q python3
+  killall -q bpftrace
+  truncate -s 0 raw.txt
+  sleep 1 # wait for 1 second
+  # run tracing if necessary
+  if [ "$2" == "X" ]; then
+    sudo bpftrace Scripts/A.bt >>raw.txt & # begin tracing
+  fi
+  # run the workload and measure how long it took
+  startTime=$SECONDS
+  # TODO : run workload
+  endTime=$SECONDS
+  # end tracing
+  killall -q bpftrace
+  # update logs
+  outputSize=$(wc -l raw.txt) # context switches recorded
+  echo "$outputSize" >>Logs/log_"$1".txt
+  echo "$startTime" >>Logs/log_"$1".txt
+  echo "$endTime" >>Logs/log_"$1".txt
 }
-
-# function to run experiment
-runExperiment() {
-    resetExperiment
-    # TODO: finish experiment
-    
-}
-
+# run experiment
+echo "Starting experiment ${1}"
+iterationCounter=0
+for _ in {1..5}; do # number of iterations
+  sleep 1
+  # experiment phase
+  iterationCounter=$((iterationCounter + 1)) && printf "\t---------Run %s---------\n" "$iterationCounter"
+  experiment "$1" X # base run
+  experiment "$1" 1 # 1 context switch
+done
+# git pull
+git add .
+git commit -m "add and process experiment $1"
+# git push # add to git
