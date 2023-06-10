048de8406f8ca49306d53e64fbda9eed3b78ce52
JawandS <jawandsingh@gmail.com>
2023-06-09 20:33:43 -0400

    process experiment

--- Stability/stabilityTest.sh
+++ Stability/stabilityTest.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# note: sudo apt install sysbench
+# sudo apt update; sudo apt upgrade -y; sudo apt autoremove -y; sudo apt install sysbench -y
+# ctrl+z; bg; disown -h
 
-for _ in {1..1500}; do
+for _ in {1..230}; do
     # log the time to file and terminal
     date | tee -a "$1".txt
     # run sysbench
-    sysbench cpu --threads=1 --time=60 --rand-seed=99 --verbosity=3 run | tee -a "$1".txt
+    sysbench cpu --threads=8 --time=60 --rand-seed=99 --verbosity=3 run | tee -a "$1".txt
 done
 
